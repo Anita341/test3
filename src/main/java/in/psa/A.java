f642ea6268d9e336ec548e567ccbe948ef744274
Anita <anita02sahu@gmail.com>
2025-03-07 11:41:57 +0530

    First Commit

--- src/main/java/in/psa/A.java
+++ src/main/java/in/psa/A.java
@@ -1,11 +1,6 @@
 package in.psa;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class A {
-    public static void main(String[] args) {
-        List<String> l = Arrays.asList("priya", "bulbul", "bijay","barsha","pankaj");
-        System.out.println(l);
-    }
+
 }
