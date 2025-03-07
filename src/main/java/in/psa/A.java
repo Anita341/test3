5ddb915e7183f52bfddde79067b37543a44f8e9e
Anita Sahu <anita02sahu@gmail.com>
2025-03-07 11:42:45 +0530

    Update A.java

--- src/main/java/in/psa/A.java
+++ src/main/java/in/psa/A.java
@@ -2,5 +2,8 @@ package in.psa;
 
 
 public class A {
-
+  public static void main(String[] args) {
+        List<String> l = Arrays.asList("priya", "bulbul", "bijay","barsha","pankaj");
+        System.out.println(l);
+    }
 }
