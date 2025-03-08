ac78428c40bd8679086034c4f632e628e51084ac
Anita <anita02sahu@gmail.com>
2025-03-08 10:15:48 +0530

    second commit

--- src/main/java/in/psa/A.java
+++ src/main/java/in/psa/A.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class A {
   public static void main(String[] args) {
-        List<String> l = Arrays.asList("priya", "bulbul", "bijay","barsha","pankaj");
+        List<Integer> l = Arrays.asList(10,20,30,40,50);
         System.out.println(l);
     }
 }
