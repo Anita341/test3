eeeba122d26953efa856474920326b2426a3eda4
Anita <anita02sahu@gmail.com>
2025-03-13 15:05:42 +0530

    delete

--- src/main/java/in/psa/A.java
+++ src/main/java/in/psa/A.java
@@ -8,14 +8,10 @@ public class A {
   public static void main(String[] args) {
         List<Integer> l = Arrays.asList(10,20,30,40,50);
         System.out.println(l);
-        int a=1;
-        int b=2;
         System.out.println("hello world");
         char x='a';
         String n="bulbul";
         boolean isPresent=true;
-      System.out.println("hiiii");
-        String y="priya";
 
     }
 }
